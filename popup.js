ebc77b79b8ffe2081dac2860beb232f0efa3fa7f
You-now-Who <yashyadav.sr@gmail.com>
2022-12-18 14:36:35 +0530

    Working Font System

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 console.log("popup.js loaded");
 
-const button = document.querySelector("button");
+const button = document.querySelector("#btn");
 button.addEventListener("click", async () => {
     console.log("button clicked");
     
@@ -8,19 +8,24 @@ button.addEventListener("click", async () => {
 
     document.getElementById("btn").innerHTML = "Clicked";
 
-    let h1 = document.getElementById("h1Font").innerHTML;
-    let h2 = document.getElementById("h2Font").innerHTML;
-    let h3 = document.getElementById("h3Font").innerHTML;
-    let h4 = document.getElementById("h4Font").innerHTML;
-    let p = document.getElementById("pFont").innerHTML;
-    let li = document.getElementById("liFont").innerHTML;
-    
+    ;
 
+    let h1Font = document.getElementById("h1Font").value;
+    let h2Font = document.getElementById("h2Font").value;
+    let h3Font = document.getElementById("h3Font").value;
+    let h4Font = document.getElementById("h4Font").value;
+    let pFont = document.getElementById("pFont").value;
+    let aFont = document.getElementById("liFont").value;
+    let liFont = document.getElementById("liFont").value;
+    let divFont = document.getElementById("divFont").value;
+
+    console.log("h1Font: ");
+    console.log (h1Font);
     chrome.runtime.sendMessage({
         msg: "buttonClicked", 
         data: {
             subject: "Button-was-clicked",
-            content: {fonts: {"span": "Times New Roman", "p": "Times New Roman", "em": "Comic Sans MS", "b": "Comic Sans MS", "i": "Comic Sans MS", "u": "Comic Sans MS", "a": "Comic Sans MS", "li": "Comic Sans MS", "td": "Comic Sans MS", "th": "Comic Sans MS", "input": "Comic Sans MS", "option": "Comic Sans MS", "h1": "Georgia", "h2": "Monaco", "h3": "Droid Sans", "h4": "Comic Sans MS", "h5": "Comic Sans MS", "h6": "Comic Sans MS", "div": "Comic Sans MS", "default": "Comic Sans MS"}}
+            content: {fonts: {"span": divFont, "p": pFont, "em": pFont, "b": pFont, "i": pFont, "u": pFont, "a": aFont, "li": liFont, "td": pFont, "th": pFont, "input": pFont, "option": liFont, "h1": h1Font, "h2": h2Font, "h3": h3Font, "h4": h4Font, "h5": h4Font, "h6": h4Font, "div": h4Font, "default": pFont}}
         }
     });
 
