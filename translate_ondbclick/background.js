4bf58d11262a59e3c6b0ef57d4d9a14f65883cb8
vyomaaverse <nandita.aka.vyomaa@gmail.com>
2022-12-18 09:07:29 +0530

    added translate function

---
+++ translate_ondbclick/background.js
@@ -0,0 +1,33 @@
+
+// addEventListener('dblclick', (event) => {
+
+
+
+// });
+
+function getSelectionText() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
+
+function onDblClick(info) {
+    const url = `http://translate.google.com/#auto/${'hindi'}/${encodeURIComponent(info.selectionText)}`;
+    ga('set', 'checkProtocolTask', () => { /* do nothing */ });
+    ga('send', {
+      hitType: 'event',
+      eventCategory: 'contextMenu',
+      eventAction: 'translate'
+    });
+  }
+  
+  chrome.action.ondblclick.addListener((tab) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: onDblClick
+    });
+  });
